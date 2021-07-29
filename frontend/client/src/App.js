390120151d3d719968793e9a06b130cc83ef095c
Adribasn <adrien.basyn@outlook.com>
2021-07-29 19:20:26 +0200

    Started build of login front end

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Switch, Route, Link } from 'react-router-dom';
+import { LoginPage } from './LoginPage';
+import { HomePage } from './HomePage';
+import { LandingPage } from './LandingPage';
+import { SignupPage } from './SignupPage';
+ 
 function App() {
   return (
     <div className="App">
-   
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/signup" component={SignupPage} />
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/home" component={HomePage} />
+      </Switch>
     </div>
   );
 }
---
+++ frontend/client/src/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const LandingPage = () => {
+    return (
+        <div className="landing-container">
+            <div className="landing-image-container">
+                LOGO
+            </div>
+
+            <div className="landing">
+                <span>LOGO</span>
+                <h1>Happening now</h1>
+                <h2>Join Baikal today.</h2>
+                <button id="signup-button">Sign up</button>
+                <button id="login-button">Log in</button>
+            </div>
+
+        </div>
+    )
+}
