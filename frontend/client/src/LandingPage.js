ea6227b18f6bea97d4c0781366e657d3d8d5d987
Adribasn <adrien.basyn@outlook.com>
2021-07-30 19:08:50 +0200

    Build desktop version of landing page

--- frontend/client/src/LandingPage.js
+++ frontend/client/src/LandingPage.js
@@ -4,15 +4,17 @@ export const LandingPage = () => {
     return (
         <div className="landing-container">
             <div className="landing-image-container">
-                LOGO
+                <div id="logo-shape-white"></div>
             </div>
 
             <div className="landing">
-                <span>LOGO</span>
+                <div id="logo-shape-colored" class="landing-section-logo"></div>
                 <h1>Happening now</h1>
                 <h2>Join Baikal today.</h2>
-                <button id="signup-button">Sign up</button>
-                <button id="login-button">Log in</button>
+                <div className="button-container">
+                    <button id="signup-button">Sign up</button>
+                    <button id="login-button">Log in</button>
+                </div>
             </div>
 
         </div>
