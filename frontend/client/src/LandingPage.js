de48157d5f44e8e58c45e900e55665857ec78d4b
Adribasn <adrien.basyn@outlook.com>
2021-08-01 18:22:39 +0200

    Started mobile first dev

--- frontend/client/src/LandingPage.js
+++ frontend/client/src/LandingPage.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import './LandingPageStyle.css';
 
 export const LandingPage = () => {
     return (
-        <div className="landing-container">
-            <div className="landing-image-container">
-                <div id="logo-shape-white"></div>
-            </div>
-
-            <div className="landing">
-                <div id="logo-shape-colored" class="landing-section-logo"></div>
-                <h1>Happening now</h1>
-                <h2>Join Baikal today.</h2>
-                <div className="button-container">
-                    <button id="signup-button">Sign up</button>
-                    <button id="login-button">Log in</button>
+        <div className="landing-grid">
+            <div className="landing-container">
+                <div class="logo-container">
+                    <div class="logo-colored"></div>
                 </div>
+
+                <h1>Happening now</h1>
+                <h2>Log in to Baikal</h2>
+                <button id="signup-button">Sign up</button>
+                <button id="login-button">Log in</button>
             </div>
 
+            <div className="image-container">
+                <div class="logo-white"></div>
+            </div>
+            
         </div>
     )
 }
