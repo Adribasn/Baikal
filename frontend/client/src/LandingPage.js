367c03fe465c7bd9ac777db3420269da9f43bc67
Adribasn <adrien.basyn@outlook.com>
2021-08-04 16:43:31 +0200

    Trying to implement logo

--- frontend/client/src/LandingPage.js
+++ frontend/client/src/LandingPage.js
@@ -3,22 +3,22 @@ import './LandingPageStyle.css';
 
 export const LandingPage = () => {
     return (
-        <div className="landing-grid">
-            <div className="landing-container">
-                <div class="logo-container">
-                    <div class="logo-colored"></div>
+        <div className="grid-container">
+            <div className="item-1">
+                <div class="logo-container-1">
+                    <img src="/home/adribas/Documents/dev/Baikal/frontend/assets/baikal-colored-text.svg"></img>
                 </div>
-
                 <h1>Happening now</h1>
-                <h2>Log in to Baikal</h2>
+                <h2>Join Baikal today.</h2>
                 <button id="signup-button">Sign up</button>
                 <button id="login-button">Log in</button>
             </div>
 
-            <div className="image-container">
-                <div class="logo-white"></div>
+            <div className="item-2">
+                <div class="logo-container-2">
+                    <div id="logo-white"></div>
+                </div>
             </div>
-            
         </div>
     )
 }
