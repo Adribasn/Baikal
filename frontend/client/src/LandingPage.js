f294c82ccda2b257d19bfe9e8039cb312a477e71
adribasn <adrien.basyn@outlook.com>
2021-08-04 17:00:40 +0200

    finished desktop v of landing

--- frontend/client/src/LandingPage.js
+++ frontend/client/src/LandingPage.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import './LandingPageStyle.css';
-import ColoredLogo from './assets/baikal-colored-text.svg';
+import ColoredLogo from './assets/baikal-colored-logo.svg';
+import WhiteLogo from './assets/baikal-white-logo.svg';
 
 export const LandingPage = () => {
     return (
         <div className="grid-container">
             <div className="item-1">
-                <div class="logo-container-1">
-                    <img src={ColoredTextLogo}></img>
-                </div>
+                <img src={ColoredLogo} id="colored-logo"></img>
                 <h1>Happening now</h1>
                 <h2>Join Baikal today.</h2>
-                <button id="signup-button">Sign up</button>
-                <button id="login-button">Log in</button>
+                <div className="button-container">
+                    <button id="signup-button">Sign up</button>
+                    <button id="login-button">Log in</button>
+                </div>
             </div>
 
             <div className="item-2">
-                <div class="logo-container-2">
-                    <div id="logo-white"></div>
-                </div>
+                <img src={WhiteLogo} id="white-logo"></img>
             </div>
         </div>
     )
