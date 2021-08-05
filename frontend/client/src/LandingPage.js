94e9d7328e67d64559d33929952661c91fbfec96
adribasn <adrien.basyn@outlook.com>
2021-08-05 15:24:51 +0200

    Finished landing page (Fully responsive v)

--- frontend/client/src/LandingPage.js
+++ frontend/client/src/LandingPage.js
@@ -2,17 +2,20 @@ import React from 'react';
 import './LandingPageStyle.css';
 import ColoredLogo from './assets/baikal-colored-logo.svg';
 import WhiteLogo from './assets/baikal-white-logo.svg';
+import { Link } from 'react-router-dom';
 
 export const LandingPage = () => {
     return (
         <div className="grid-container">
             <div className="item-1">
-                <img src={ColoredLogo} id="colored-logo"></img>
-                <h1>Happening now</h1>
-                <h2>Join Baikal today.</h2>
-                <div className="button-container">
-                    <button id="signup-button">Sign up</button>
-                    <button id="login-button">Log in</button>
+                <div class="item-1-inner-container">
+                    <img src={ColoredLogo} id="colored-logo"></img>
+                    <h1>Happening now</h1>
+                    <h2>Join Baikal today.</h2>
+                    <div className="button-container">
+                        <Link to="/signup" style={{ textDecoration: 'none' }}><button id="signup-button">Sign up</button></Link>
+                        <Link to="/login" style={{ textDecoration: 'none' }}><button id="login-button">Log in</button></Link>
+                    </div>
                 </div>
             </div>
 
