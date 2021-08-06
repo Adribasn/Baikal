e16cf5500505f2866f995118c79f231d8bd5c59f
adribasn <adrien.basyn@outlook.com>
2021-08-06 17:02:58 +0200

    Landing, Login and Signup page all fully responsive.

--- frontend/client/src/LoginPage.js
+++ frontend/client/src/LoginPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import LoginCSS from './LoginPageStyle.module.css';
+import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
     return (
-        <div>
-            
+        <div className={LoginCSS.gridContainer}>
+            <div className={LoginCSS.itemOneContainer}>
+                <div className={LoginCSS.itemOne}>
+                    <div>
+                        <h1 className={LoginCSS.welcome}>Welcome</h1>
+                        <div className={LoginCSS.inputContainer}>
+                            <input className={LoginCSS.email} placeholder="Email"></input>
+                            <input className={LoginCSS.password} placeholder="password" type="password"></input>
+                        </div>
+                        <Link to="/home" style={{ textDecoration: 'none' }}><button className={LoginCSS.loginButton}>Login</button></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/client/src/SignupPage.js
+++ frontend/client/src/SignupPage.js
@@ -8,11 +8,12 @@ export const SignupPage = () => {
             <div className={SignupCSS.itemOneContainer}>
                 <div className={SignupCSS.itemOne}>
                     <div>
-                        <h1>Sign up</h1>
+                        <h1 className={SignupCSS.signupText}>Sign up</h1>
                         <div className={SignupCSS.inputContainer}>
                             <input className={SignupCSS.firstName} placeholder="First name"></input>
                             <input className={SignupCSS.lastName} placeholder="Last name"></input>
                             <input className={SignupCSS.email} placeholder="Email"></input>
+                            <input className={SignupCSS.password} placeholder="password" type="password"></input>
                         </div>
                         <Link to="/home" style={{ textDecoration: 'none' }}><button className={SignupCSS.registerButton}>Register</button></Link>
                     </div>
