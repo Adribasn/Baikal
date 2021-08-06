80b3ecaee326bac8157f59360f9ef38585983e94
adribasn <adrien.basyn@outlook.com>
2021-08-06 15:01:57 +0200

    Landing page responsive and signup desktop v done

--- frontend/client/src/SignupPage.js
+++ frontend/client/src/SignupPage.js
@@ -5,13 +5,17 @@ import SignupCSS from './SignupPageStyle.module.css';
 export const SignupPage = () => {
     return (
         <div className={SignupCSS.gridContainer}> 
-            <div className={SignupCSS.itemOne}>
-                <div>
-                    <h1>Sign up</h1>
-                    <input className={SignupCSS.firstName} placeholder="First name"></input>
-                    <input className={SignupCSS.lastName} placeholder="Last name"></input>
-                    <input className={SignupCSS.email} placeholder="Email"></input>
-                    <button className={SignupCSS.registerButton}>Register</button>
+            <div className={SignupCSS.itemOneContainer}>
+                <div className={SignupCSS.itemOne}>
+                    <div>
+                        <h1>Sign up</h1>
+                        <div className={SignupCSS.inputContainer}>
+                            <input className={SignupCSS.firstName} placeholder="First name"></input>
+                            <input className={SignupCSS.lastName} placeholder="Last name"></input>
+                            <input className={SignupCSS.email} placeholder="Email"></input>
+                        </div>
+                        <Link to="/home" style={{ textDecoration: 'none' }}><button className={SignupCSS.registerButton}>Register</button></Link>
+                    </div>
                 </div>
             </div>
         </div>
